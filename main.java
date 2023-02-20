7576b84ea03e8f4f2a55693f48627409214548a5
tailoryash <theyash.tailor@gmail.com>
2023-02-20 09:30:36 +0530

    few changes in main.java file - rebase

--- main.java
+++ main.java
@@ -3,6 +3,8 @@ class Main{
         int a = 10;
         int b = 20;
 
-        System.out.println("A*B", a*b);
+        System.out.println("A+B", a+b);
+
+        //few changes for commit
     }
 }
